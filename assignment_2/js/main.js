dc046304e139e65d7319d5eef40c792099362585
caioob <caio.basaglia@outlook.com>
2019-06-14 20:28:23 -0400

    a2 done

--- assignment_2/js/main.js
+++ assignment_2/js/main.js
@@ -2,22 +2,26 @@ let employeesModel = [];
 
 $(function () {
     initializeEmployeesModel();
-    $("employee-search").on("keyup", () =>{
-        let search = getFilteredEmployeesModel(this.value);
-        console.log(search);
-        refreshEmployeeRows();
+    $("#employee-search").on("keyup", (data) =>{
+        let employee = getFilteredEmployeesModel(data.target.value);
+        refreshEmployeeRows(employee);
     });
 
-    $("body-row").on("click", () => {
-        let returnedEmp = getEmployeeModelById($(this).attr("data-id"));
-        returnedEmp.HireDate = moment(returnedEmp.HireDate).format("ll");
-        let template = _.template(
-            '<strong>Address:</strong> <%- employee.AddressStreet %> <%- employee.AddressCity %>, <%- employee.AddressState %>. <%- employee.AddressZip %></br>' + 
-            '<strong>Phone Number:</strong> <%- employee.PhoneNum %> ext: <%- employee.Extension %></br>' +
-            '<strong>Hire Date:</strong> <%- employee.HireDate %>' 
-        );
-        let content = template(({'employees': returnedEmp}));
-        showGenericModalfunction(returnedEmp.FirstName + " " + returnedEmp.LastName, content);
+    $(document.body).on('click', '.body-row' ,function(empl){
+        let employee = getEmployeeModelById($(this).attr("data-id"));
+        if(employee != null){
+            employee.HireDate = moment(employee.HireDate).format('L');
+            
+            let modalTemplate = _.template(
+                '<strong>Address:</strong> <%- employee.AddressStreet %> <%- employee.AddressCity %>, <%- employee.AddressState %>. <%- employee.AddressZip %></br>' + 
+                '<strong>Phone Number:</strong> <%- employee.PhoneNum %> ext: <%- employee.Extension %></br>' +
+                '<strong>Hire Date:</strong> <%- employee.HireDate %>' 
+              );
+
+              let modalValues = modalTemplate({'employee':employee});
+   
+              showGenericModal(employee.FirstName + ' ' + employee.LastName, modalValues);
+        }
     });
 });
 
@@ -34,43 +38,44 @@ function initializeEmployeesModel(){
     .fail(() =>{
         showGenericModal('Error', 'Unable to get Employees');
     })
-}
+};
 
 function showGenericModal(title,message){
     $("#genericModal .modal-tile").empty().append(title);
     $("#genericModal .modal-body").empty().append(message);
     $("#genericModal").modal('show');
-}
+};
 
 function refreshEmployeeRows(employees){
     $("#employees-table").empty();
     let template= _.template('<% _.forEach(employees, function(employee){%>' 
-                                +'<div class="row body-row" data-id="<% employee._id%>">'
-                                    +'<div class="col-xs-4 body-column"><%_.escape(employee.firstName)%></div>'
-                                    +'<div class="col-xs-4 body-column"><%_.escape(employee.lastName)%></div>'
-                                    +'<div class="col-xs-4 body-column"><%_.escape(employee.positionName)%></div>'
+                                +'<div class="row body-row" data-id="<%- employee._id%>">'
+                                    +'<div class="col-xs-4 body-column" style="text-align: center"><%- _.escape(employee.FirstName)%></div>'
+                                    +'<div class="col-xs-4 body-column" style="text-align: center"><%- _.escape(employee.LastName)%></div>'
+                                    +'<div class="col-xs-4 body-column" style="text-align: center"><%- _.escape(employee.Position.PositionName)%></div>'
                                 +'</div>'
                                 +'<% }); %>');
     $("#employees-table").append(template(({'employees': employees})));
-}
+};
 
-function getFilteredEmployeesModel(filterString) {
-    let filteredArray = _.filter(employeesModel, function(empModel){
-        return (empModel.LastName.toUpperCase().includes(filterString) || 
-                empModel.FirstName.toUpperCase().includes(filterString) ||
-                empModel.Position.PositionName.toUpperCase().includes(filterString));
+
+function getFilteredEmployeesModel(filterString){
+    let filteredEmployeesModel = _.filter(employeesModel, (employee) => {
+        return employee.FirstName.toUpperCase().includes(filterString.toUpperCase())
+            || employee.LastName.toUpperCase().includes(filterString.toUpperCase())
+            || employee.Position.PositionName.toUpperCase().includes(filterString.toUpperCase());          
     });
-    return filteredArray;
-}
+    return filteredEmployeesModel;        
+};
 
 function getEmployeeModelById(id){
-    let employeesReturn = null;
-    $.grep(employeesModel, function(employee, i) {
+    let copy;
+    $.grep(employeesModel, (employee) => {
         if(employee._id == id) {
-            employeesReturn = _.cloneDeep(employee);
+            copy = _.cloneDeep(employee);
         }
         return false;
     });
-    return employeesReturn;
-}
-
+    
+    return copy;
+};
